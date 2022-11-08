113b22f5690163b452cf32c93d12cf0bc158fb97
Lafarizo <88442671+Lafarizo@users.noreply.github.com>
2022-11-09 03:38:00 +0700

    Add files via upload

--- bbp_ibmn.sql
+++ bbp_ibmn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 08, 2022 at 03:50 PM
+-- Generation Time: Nov 08, 2022 at 09:35 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.1.6
 
@@ -26,152 +26,345 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `admisi`
 --
--- Error reading structure for table bbp_ibmn.admisi: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;admisi&#039;
--- Error reading data for table bbp_ibmn.admisi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`admisi`&#039; at line 1
+
+CREATE TABLE `admisi` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `bahasa`
 --
--- Error reading structure for table bbp_ibmn.bahasa: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;bahasa&#039;
--- Error reading data for table bbp_ibmn.bahasa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`bahasa`&#039; at line 1
+
+CREATE TABLE `bahasa` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `bisnis`
 --
--- Error reading structure for table bbp_ibmn.bisnis: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;bisnis&#039;
--- Error reading data for table bbp_ibmn.bisnis: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`bisnis`&#039; at line 1
+
+CREATE TABLE `bisnis` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `faib`
 --
--- Error reading structure for table bbp_ibmn.faib: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;faib&#039;
--- Error reading data for table bbp_ibmn.faib: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`faib`&#039; at line 1
+
+CREATE TABLE `faib` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `fdk`
 --
--- Error reading structure for table bbp_ibmn.fdk: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;fdk&#039;
--- Error reading data for table bbp_ibmn.fdk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`fdk`&#039; at line 1
+
+CREATE TABLE `fdk` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `febi`
 --
--- Error reading structure for table bbp_ibmn.febi: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;febi&#039;
--- Error reading data for table bbp_ibmn.febi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`febi`&#039; at line 1
+
+CREATE TABLE `febi` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `fish`
 --
--- Error reading structure for table bbp_ibmn.fish: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;fish&#039;
--- Error reading data for table bbp_ibmn.fish: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`fish`&#039; at line 1
+
+CREATE TABLE `fish` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `fitk`
 --
--- Error reading structure for table bbp_ibmn.fitk: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;fitk&#039;
--- Error reading data for table bbp_ibmn.fitk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`fitk`&#039; at line 1
+
+CREATE TABLE `fitk` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `fsh`
 --
--- Error reading structure for table bbp_ibmn.fsh: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;fsh&#039;
--- Error reading data for table bbp_ibmn.fsh: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`fsh`&#039; at line 1
+
+CREATE TABLE `fsh` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `fst`
 --
--- Error reading structure for table bbp_ibmn.fst: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;fst&#039;
--- Error reading data for table bbp_ibmn.fst: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`fst`&#039; at line 1
+
+CREATE TABLE `fst` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `fupi`
 --
--- Error reading structure for table bbp_ibmn.fupi: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;fupi&#039;
--- Error reading data for table bbp_ibmn.fupi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`fupi`&#039; at line 1
+
+CREATE TABLE `fupi` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `lp2m`
 --
--- Error reading structure for table bbp_ibmn.lp2m: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;lp2m&#039;
--- Error reading data for table bbp_ibmn.lp2m: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`lp2m`&#039; at line 1
+
+CREATE TABLE `lp2m` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `lpm`
 --
--- Error reading structure for table bbp_ibmn.lpm: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;lpm&#039;
--- Error reading data for table bbp_ibmn.lpm: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`lpm`&#039; at line 1
+
+CREATE TABLE `lpm` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `pasca`
 --
--- Error reading structure for table bbp_ibmn.pasca: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;pasca&#039;
--- Error reading data for table bbp_ibmn.pasca: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`pasca`&#039; at line 1
+
+CREATE TABLE `pasca` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `pau`
 --
--- Error reading structure for table bbp_ibmn.pau: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;pau&#039;
--- Error reading data for table bbp_ibmn.pau: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`pau`&#039; at line 1
+
+CREATE TABLE `pau` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `perpus`
 --
--- Error reading structure for table bbp_ibmn.perpus: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;perpus&#039;
--- Error reading data for table bbp_ibmn.perpus: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`perpus`&#039; at line 1
+
+CREATE TABLE `perpus` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `ptipd`
 --
--- Error reading structure for table bbp_ibmn.ptipd: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;ptipd&#039;
--- Error reading data for table bbp_ibmn.ptipd: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`ptipd`&#039; at line 1
+
+CREATE TABLE `ptipd` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `spi`
 --
--- Error reading structure for table bbp_ibmn.spi: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;spi&#039;
--- Error reading data for table bbp_ibmn.spi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`spi`&#039; at line 1
+
+CREATE TABLE `spi` (
+  `ruang` varchar(30) NOT NULL,
+  `kd_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `pemakai` varchar(100) NOT NULL,
+  `kondisi` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `table_user`
 --
--- Error reading structure for table bbp_ibmn.table_user: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;table_user&#039;
--- Error reading data for table bbp_ibmn.table_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bbp_ibmn`.`table_user`&#039; at line 1
+
+CREATE TABLE `table_user` (
+  `kd_user` varchar(7) NOT NULL,
+  `nama_user` varchar(20) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `foto_user` varchar(50) NOT NULL,
+  `level` enum('Admin','Kasir','Manager') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `table_user`
+--
+
+INSERT INTO `table_user` (`kd_user`, `nama_user`, `username`, `password`, `foto_user`, `level`) VALUES
+('US001', 'Informatika15', 'informatika15', 'aW5mb3JtYXRpa2ExNQ==', '15.png', 'Admin'),
+('US002', 'Hasan Basri', 'hasanbasri', 'YWRtaW4=', '153777087384.png', 'Admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fst`
+--
+ALTER TABLE `fst`
+  ADD PRIMARY KEY (`kd_barang`);
+
+--
+-- Indexes for table `table_user`
+--
+ALTER TABLE `table_user`
+  ADD PRIMARY KEY (`kd_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
